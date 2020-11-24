9009a20b93110cf0538135a9397f733b7ff4328b
Jim Counts <jamesrcounts@outlook.com>
2020-11-23 20:37:50 -0800

    Update versions.tf

--- infrastructure/azure/versions.tf
+++ infrastructure/azure/versions.tf
@@ -15,6 +15,14 @@ terraform {
       version = "~> 3.0.0"
     }
   }
+  
+    backend "remote" {
+    organization = "jamesrcounts"
+
+    workspaces {
+      name = "private-aks"
+    }
+  }
 }
 
 provider azurerm {
