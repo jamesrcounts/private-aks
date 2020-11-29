44aa46d2b7dec1697d27a8d808cd79dda8b1a5a6
Jim Counts <jamesrcounts@outlook.com>
2020-11-29 16:40:20 +0000

    Fork of loafsley

--- go/src/github.com/jamesrcounts/terraform-provider-azprivatedns/go.mod
+++ go/src/github.com/jamesrcounts/terraform-provider-azprivatedns/go.mod
@@ -3,6 +3,8 @@ module terraform-provider-azprivatedns
 go 1.13
 
 require (
+	github.com/Azure/azure-sdk-for-go v47.1.0+incompatible
+	github.com/hashicorp/go-azure-helpers v0.13.1
 	github.com/hashicorp/terraform-plugin-sdk v1.16.0
-	github.com/hashicorp/terraform-plugin-sdk/v2 v2.3.0
+	github.com/satori/go.uuid v1.2.0
 )
--- go/src/github.com/jamesrcounts/terraform-provider-azprivatedns/main.go
+++ go/src/github.com/jamesrcounts/terraform-provider-azprivatedns/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-  "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-  "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-
-  "terraform-provider-azprivatedns/azprivatedns"
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"terraform-provider-azprivatedns/azprivatedns"
 )
 
 func main() {
-  plugin.Serve(&plugin.ServeOpts{
-    ProviderFunc: func() *schema.Provider {
-      return azprivatedns.Provider()
-    },
-  })
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() terraform.ResourceProvider {
+			return azprivatedns.Provider()
+		},
+	})
 }
