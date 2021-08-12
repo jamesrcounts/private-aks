08a23f02e9113f49719d8ba525d4328da19d50f9
Jim Counts <jamesrcounts@outlook.com>
2021-08-12 13:52:27 +0000

    Update firewall call site

--- infrastructure/azure/firewall.tf
+++ infrastructure/azure/firewall.tf
@@ -1,7 +1,12 @@
 module "firewall" {
   source = "../modules/firewall"
 
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
-  resource_group             = data.azurerm_resource_group.net
-  subnet_id                  = module.networks.subnets["firewall"].id
+  resource_group = data.azurerm_resource_group.net
+  subnet_id      = module.networks.subnets["firewall"].id
+  log_analytics_workspace = merge(
+    azurerm_log_analytics_workspace.insights,
+    {
+      subscription_id = data.azurerm_client_config.current.subscription_id
+    }
+  )
 }
