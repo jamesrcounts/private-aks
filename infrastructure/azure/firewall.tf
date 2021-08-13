1e098b6f8d1a46a2f3a735aae69c9dda11c7fe8a
Jim Counts <jamesrcounts@outlook.com>
2021-08-13 13:51:14 +0000

    Add policy to firewall

--- infrastructure/azure/firewall.tf
+++ infrastructure/azure/firewall.tf
@@ -1,12 +1,19 @@
 module "firewall" {
   source = "github.com/jamesrcounts/private-aks.git//infrastructure/modules/firewall?ref=lz"
 
-  resource_group = data.azurerm_resource_group.net
-  subnet_id      = module.networks.subnets["firewall"].id
+  firewall_policy_id = module.firewall_policy.firewall_policy_id
+  resource_group     = data.azurerm_resource_group.net
+  subnet_id          = module.networks.subnets["firewall"].id
   log_analytics_workspace = merge(
     azurerm_log_analytics_workspace.insights,
     {
       subscription_id = data.azurerm_client_config.current.subscription_id
     }
   )
+}
+
+module "firewall_policy" {
+  source = "github.com/jamesrcounts/private-aks.git//infrastructure/modules/firewall-policy-aks?ref=lz"
+
+  resource_group = data.azurerm_resource_group.net
 }
