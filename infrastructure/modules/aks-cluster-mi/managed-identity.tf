61249defbad95c78409f55429941369278511bce
Jim Counts <jamesrcounts@outlook.com>
2021-08-14 14:59:22 +0000

    Add AKS cluster MI

---
+++ infrastructure/modules/aks-cluster-mi/managed-identity.tf
@@ -0,0 +1,21 @@
+// az identity create --name $KUBE_NAME --resource-group $KUBE_GROUP
+resource "azurerm_user_assigned_identity" "aks" {
+  location            = var.resource_group.location
+  name                = "uai-aks-${local.instance_id}"
+  resource_group_name = var.resource_group.name
+  tags                = var.resource_group.tags
+}
+
+
+// MSI_RESOURCE_ID=$(az identity show -n $KUBE_NAME -g $KUBE_GROUP -o json | jq -r ".id")
+// MSI_CLIENT_ID=$(az identity show -n $KUBE_NAME -g $KUBE_GROUP -o json | jq -r ".clientId")
+// az role assignment create --role "Virtual Machine Contributor" --assignee $MSI_CLIENT_ID -g $VNET_GROUP
+resource "azurerm_role_assignment" "aks_vnet_contributor" {
+  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+  role_definition_name = "Virtual Machine Contributor"
+  scope                = var.scope
+}
+
+
+
+
---
+++ infrastructure/modules/aks-cluster-mi/variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group" {
+  description = "(Required) The resource group to deploy the policy into."
+  type = object({
+    name     = string
+    location = string
+    tags     = map(string)
+  })
+}
+
+variable "scope" {
+  description = "(Required) The scope at which the role assignments apply to."
+  type        = string
+}
