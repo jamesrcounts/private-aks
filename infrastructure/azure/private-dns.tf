233dee2f8ae7196310a55813def59e6fe7b55fdd
Jim Counts <jamesrcounts@outlook.com>
2021-08-14 17:33:44 +0000

    Associate private dns zone

--- infrastructure/azure/private-dns.tf
+++ infrastructure/azure/private-dns.tf
@@ -1,5 +1,19 @@
 resource "azurerm_private_dns_zone" "azmk8s" {
-  name                = "privatelink.eastus2.azmk8s.io"
+  name                = "privatelink.${data.azurerm_resource_group.net.location}.azmk8s.io"
   resource_group_name = data.azurerm_resource_group.net.name
   tags                = data.azurerm_resource_group.net.tags
 }
+
+resource "azurerm_private_dns_zone_virtual_network_link" "private_link_to_hub" {
+  name                  = "hub"
+  resource_group_name   = data.azurerm_resource_group.net.name
+  private_dns_zone_name = azurerm_private_dns_zone.azmk8s.name
+  virtual_network_id    = module.networks.networks["hub"].id
+}
+
+
+
+
+
+
+
