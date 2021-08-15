5f8a1a604c4ed4cdc7207adfc21f507c8a21cf3f
Jim Counts <jamesrcounts@outlook.com>
2021-08-15 16:25:05 +0000

    Update firewall dashboard template

--- infrastructure/modules/firewall/dashboard.tf
+++ infrastructure/modules/firewall/dashboard.tf
@@ -1,11 +1,12 @@
 # Imported from: https://github.com/Azure/Azure-Network-Security/blob/59f3913c192c74718fb1cab675f32c3b375da8a4/Azure%20Firewall/Workbook%20-%20Azure%20Firewall%20Monitor%20Workbook/Azure%20Firewall_ARM.json
 # License: https://github.com/Azure/Azure-Network-Security/blob/59f3913c192c74718fb1cab675f32c3b375da8a4/LICENSE
-resource "azurerm_template_deployment" "workbook" {
+resource "azurerm_resource_group_template_deployment" "workbook" {
   name                = "wb-azure-firewall-${local.instance_id}"
   resource_group_name = var.resource_group.name
   deployment_mode     = "Incremental"
+  tags                = var.resource_group.tags
 
-  template_body = templatefile(
+  template_content = templatefile(
     "${path.module}/workbooks/AzureFirewall.arm.json.hcl",
     {
       workspace_resource_group_name = var.log_analytics_workspace.resource_group_name
