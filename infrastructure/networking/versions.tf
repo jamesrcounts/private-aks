5e8828097de3a03f5a4284388703ac7c655793c0
Jim Counts <jamesrcounts@outlook.com>
2021-08-16 14:16:32 +0000

    Add networks to base

---
+++ infrastructure/networking/versions.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1"
+
+  backend "azurerm" {}
+
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+      purge_soft_delete_on_destroy    = true
+    }
+
+    template_deployment {
+      delete_nested_items_during_deletion = true
+    }
+
+    virtual_machine {
+      delete_os_disk_on_deletion = true
+    }
+
+    virtual_machine_scale_set {
+      roll_instances_when_required = true
+    }
+  }
+}
