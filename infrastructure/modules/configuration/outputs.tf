9307aee691a3fbe9f77760edea3e107d54c8ba71
Jim Counts <jamesrcounts@outlook.com>
2021-08-16 14:27:59 +0000

    Add log analytics to configuration

--- infrastructure/modules/configuration/outputs.tf
+++ infrastructure/modules/configuration/outputs.tf
@@ -1,7 +1,7 @@
 output "instance_id" {
   value = {
     backend = data.azurerm_resource_group.backend.tags["instance_id"]
-    hub     = data.azurerm_key_vault_secret.values["hub-instance-id"].value
-    spoke   = data.azurerm_key_vault_secret.values["spoke-instance-id"].value
+    hub     = nonsensitive(data.azurerm_key_vault_secret.values["hub-instance-id"].value)
+    spoke   = nonsensitive(data.azurerm_key_vault_secret.values["spoke-instance-id"].value)
   }
 }
