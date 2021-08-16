6f7fe994ca62d4458ad24bbdda0babe67a604876
Jim Counts <jamesrcounts@outlook.com>
2021-08-16 15:00:15 +0000

    Wire up route table

--- infrastructure/networking/firewall.tf
+++ infrastructure/networking/firewall.tf
@@ -4,6 +4,7 @@ module "firewall" {
   firewall_policy_id  = module.firewall_policy.firewall_policy_id
   public_ip_prefix_id = module.networks.public_ip_prefix_id
   resource_group      = data.azurerm_resource_group.net
+  route_table         = module.networks.route_table
   subnet_id           = module.networks.subnets["firewall"].id
 
   log_analytics_workspace = merge(
