9ae2917e6fbf36a54769190f6b181cdf0aea74bf
Jim Counts <jamesrcounts@outlook.com>
2021-08-17 14:49:40 +0000

    Add depends on

--- infrastructure/appenv/aks.tf
+++ infrastructure/appenv/aks.tf
@@ -1,6 +1,8 @@
 module "aks_cluster" {
   source = "github.com/jamesrcounts/private-aks.git//infrastructure/modules/aks-private-cluster?ref=lz"
 
+  depends_on = [module.aks_cluster_identity]
+
   admin_group_object_id      = azuread_group.aks_administrators.object_id
   log_analytics_workspace_id = module.configuration.log_analytics_workspace.id
   resource_group             = data.azurerm_resource_group.main
